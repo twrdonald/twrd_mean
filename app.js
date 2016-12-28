d4c78b629e8c59b3267c87e258f6d6cb66c9f106
Tom Donald <TD@Toms-iMac.local>
2016-12-28 17:43:08 +0000

    revert

--- app.js
+++ app.js
@@ -10,22 +10,16 @@ app.factory('posts', [function(){
 }])
 //
 app.controller('MainCtrl', [
-'$scope','posts',
+'$scope',
 function($scope){
   $scope.test = 'Hello world!';
- 
- 
- /* replacing dummy with $scope.posts = posts.posts;
+  
   $scope.posts = [
   {title: 'post 1', upvotes: 5},
   {title: 'post 2', upvotes: 2},
   {title: 'post 3', upvotes: 15},
   {title: 'post 4', upvotes: 9},
   {title: 'post 5', upvotes: 4}
-
-*/
-$scope.posts = posts.posts;
-
 ];
 
 // add post
